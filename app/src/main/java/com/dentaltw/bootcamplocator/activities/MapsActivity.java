e7f06dc26e7bec4bb097ed6199a5566ae5801d03
Scott <mymislife@gmail.com>
2017-04-24 17:12:50 +0800

    add permission to this app

--- app/src/main/java/com/dentaltw/bootcamplocator/activities/MapsActivity.java
+++ app/src/main/java/com/dentaltw/bootcamplocator/activities/MapsActivity.java
@@ -1,23 +1,113 @@
 package com.dentaltw.bootcamplocator.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import com.dentaltw.bootcamplocator.R;
 import com.dentaltw.bootcamplocator.fragments.MainFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class MapsActivity extends FragmentActivity{
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
+    final int PERMISSION_LOCATION = 111;
+    private GoogleApiClient mGoogleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+
         MainFragment mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.container_main);
-        if(mainFragment==null){
+        if (mainFragment == null) {
             mainFragment = MainFragment.newInstance();
             getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.container_main, mainFragment)
-                .commit();
+                    .beginTransaction()
+                    .add(R.id.container_main, mainFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.v("MAPS","Long: "+location.getLongitude()+" - Lat: "+location.getLatitude());
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        String access_fine = Manifest.permission.ACCESS_FINE_LOCATION;
+        if(ContextCompat.checkSelfPermission(this, access_fine) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{access_fine},PERMISSION_LOCATION);
+            Log.v("MAPS", "Requesting permissions");
+        }
+        else{
+            Log.v("MAPS", "Starting Location Services from onConnected");
+            startLocationServices();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_LOCATION:{
+                boolean isLocationPermissionGranted = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                if (isLocationPermissionGranted) {
+                    startLocationServices();
+                    Log.v("MAPS", "Permission Granted - starting services");
+                }
+                else{
+                    Log.v("MAPS", "Permission not Granted");
+                }
+            }
+        }
+    }
+
+    public void startLocationServices() {
+        Log.v("MAPS", "Starting Location Services Called");
+        try {
+            LocationRequest req = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, req, this);
+            Log.v("MAPS", "Requesting Location updates");
+        } catch (SecurityException ex) {
+            Log.v("MAPS", ex.toString());
         }
     }
 }
